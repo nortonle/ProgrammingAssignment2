6d6e6a5e30fce3b92e252370a56926dec7caa329
NortonLe <norton0704@yahoo.co.uk>
2015-09-26 01:10:19 +0700

    adding comment to the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##makeCacheMatrix has 4 sub functions, which are get, set, getInverse and setInverse
+##get allows users to modify the orignial matrix
+##get returns the orignial matrix that the function contains
+##setInverse allows users to cache the original matrix's inverse matrix. Initially, this is null.
+##getInverse returns the inverse matrix, if there is any.
 makeCacheMatrix <- function(x = matrix()) 
 {
   ix <- NULL
@@ -28,11 +28,10 @@ makeCacheMatrix <- function(x = matrix())
 }
 
 
-## Write a short comment describing this function
-
+##cacheSolve returns the cached inverse matrix if there is any. If there is no inverse matrix, it will solve one.
 cacheSolve <- function(x, ...) 
 {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   ix <- x$getInverse()
   if (!is.null(ix))
   {
@@ -40,6 +39,7 @@ cacheSolve <- function(x, ...)
     return(ix)
   }
   
+  ##If the function detects there is no cached data, it begins solving inverse matrix.
   v_data <- x$get()
   ix <- solve(v_data)
   x$setInverse(ix)
